d349856cfc6cbcf9202988ff380968567b6d75f8
BardoftheOzarks <1886@holbertonschool.com>
2021-02-04 23:46:21 +0000

    moved allow_redirects

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,9 @@ Returns number of subscribers for a specific subreddit
 
 def number_of_subscribers(subreddit):
     from requests import get
-    headers = {'user-agent': 'user', 'allow_redirect': 'FALSE'}
+    headers = {'user-agent': 'user'}
     URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    data = get(URL, headers=headers).json()
+    data = get(URL, headers=headers, allow_redirects=False).json()
     subdata = data.get('data')
     subscribers = subdata.get('subscribers')
     if type(subscribers) is not int:
