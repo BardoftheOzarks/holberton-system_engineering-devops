b893d8a493f08c6c15ab646b2fe82235d461ddc6
BardoftheOzarks <1886@holbertonschool.com>
2021-02-04 23:33:51 +0000

    initial push

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Returns number of subscribers for a specific subreddit
+'''
+def number_of_subscribers(subreddit):
+    from requests import get
+    headers = {'user-agent': 'user'}
+    URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    data = get(URL, headers=headers).json()
+    subdata = data.get('data')
+    subscribers = subdata.get('subscribers')
+    if subscribers is None:
+        return 0
+    return subscribers
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+Directory for the API Advanced project tasks
