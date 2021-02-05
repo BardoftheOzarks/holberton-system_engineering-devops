7c271378077767dce8e07ab194e35d153d33135c
BardoftheOzarks <1886@holbertonschool.com>
2021-02-05 02:43:04 +0000

    worked around pagination

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,17 +4,21 @@ Returns list of hot posts for a specific subreddit recursively
 '''
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     from requests import get
     headers = {'user-agent': 'user'}
-    URL = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    data = get(URL, headers=headers, allow_redirects=False)
+    URL = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(subreddit,
+                                                                  after)
+    data = get(URL, headers=headers, allow_redirects=False,)
     if data.status_code is not 200:
         return None
     else:
         datalist = data.json().get('data').get('children')
-        subdata = datalist[len(hot_list) - 1].get('data').get('title')
-        if subdata not in hot_list:
-            hot_list.append(subdata)
-            recurse(subreddit, hot_list)
-            return hot_list
+        for entry in datalist:
+            subdata = entry.get('data').get('title')
+            if subdata not in hot_list:
+                hot_list.append(subdata)
+        after = data.json().get('data').get('after')
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return hot_list
