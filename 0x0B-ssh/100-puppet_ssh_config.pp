c9049de0b81a96a0aa543800cffa1b6a57656558
BardoftheOzarks <1886@holbertonschool.com>
2021-04-06 15:58:42 +0000

    linted

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Configures SSH to connect without a password
 file_line { 'Turn off passwd auth' :
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  match   => 'PasswordAuthentication',
-  line    => 'PasswordAuthentication no'
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  match  => 'PasswordAuthentication',
+  line   => 'PasswordAuthentication no'
 }
 
 file_line { 'Declare identity file' :
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/holberton'
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton'
 }
