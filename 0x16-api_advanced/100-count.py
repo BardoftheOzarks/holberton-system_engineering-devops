e04c8e0cf6b07641ad6e1ae72360d65d5c906bd9
BardoftheOzarks <1886@holbertonschool.com>
2021-05-19 23:51:10 -0500

    added advanced

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''queries the Reddit API'''
+import json
+import requests
+
+
+def count_words(subreddit, word_list, after=None, counts={}):
+    '''returns list of hot posts recursively'''
+    if after is None:
+        response = requests.get('https://api.reddit.com/r/{}/hot'
+                                .format(subreddit),
+                                headers={'user-agent': 'Excelsior'},
+                                allow_redirects=False).json()
+    else:
+        response = requests.get('https://api.reddit.com/r/{}/hot?after={}'
+                                .format(subreddit, after),
+                                headers={'user-agent': 'Excelsior'},
+                                allow_redirects=False).json()
+    for word in word_list:
+        word = word.lower()
+        if word not in counts.keys():
+            counts[word] = 0
+    data = response.get('data')
+    children = data.get('children')
+    for child in children:
+        subdata = child.get('data')
+        title = subdata.get('title')
+        title = title.split(' ')
+        for word in word_list:
+            counts[word] += title.count(word)
+    after = data.get('after')
+    if after is None:
+        results = []
+        for key, value in sorted(counts.items(), key=lambda item: item[1]):
+            if value != 0:
+                results.append('{}: {}'.format(key, value))
+        x = len(results)
+        while x > 0:
+            x -= 1
+            print(results[x])
+        return
+    return count_words(subreddit, word_list, after, counts)
